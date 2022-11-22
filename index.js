f5f70a594299e54078cad0e83bf62ef8fbedb8c1
regular-meet-8 <eddiekim444@gmail.com>
2022-11-22 13:10:43 +0300

    done arithmeticlab

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b){
+
+return  a+b
+}
+function subtract(a, b){
+      return a-b
+}
+function divide(a, b){
+    return  a/b
+    
+}
+function multiply(a, b){
+         return a*b
+
+}
+function increment(){
+    n= 32
+    return n++
+}
+function decrement(n){
+    n=728
+    return n--
+}
+function makeInt(n){
+    return makeInt(n).toString().toEqual(0)
+}
+function preserveDecimal(n){
+    return   parseFloat(n)
+}
+
